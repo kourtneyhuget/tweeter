b196e60bd1ffb1cf12006eda0d7adec505751160
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-10-08 01:13:48 +0000

    added in loadTweets function so that new tweets are posting to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,33 +5,55 @@
  */
 
 $(document).ready(function () {
-  renderTweets(data);
+  $('#tweet-form').on('submit', submitHandler);
+  loadtweets();
 });
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+const loadtweets = function () {
+  $.ajax('/tweets/', { method: 'GET' })
+    .then(function (res, err) {
+      renderTweets(res);
+    });
+};
+
+const submitHandler = function (event) {
+  event.preventDefault();
+  const $form = $(this);
+  const text = $form.serialize();
+  $.ajax({
+    method: "POST",
+    url: "/tweets/",
+    data: text
+  }).then(function () {
+    loadtweets();
+    console.log('Successfully submitted');
+  });
+};
+
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 const renderTweets = function (tweets) {
   const $container = $(".tweets-container");
